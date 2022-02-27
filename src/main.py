2333236ddcc6b78a1fa1ac6a7e8b045bf20184ac
sheepc <970973835@qq.com>
2022-02-27 14:28:31 +0800

    option

--- src/main.py
+++ src/main.py
@@ -35,5 +35,5 @@ def train():
 if __name__ == '__main__':
     LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
     DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
-    logging.basicConfig(filename='my.log', level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+    logging.basicConfig(filename='my.log', level=logging.WARN, format=LOG_FORMAT, datefmt=DATE_FORMAT)
     train()
